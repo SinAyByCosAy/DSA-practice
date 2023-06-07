048dd67929e975e5ed11428b64aef960c3df39e6
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-07 17:17:21 +0530

    Refactored arch

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,35 @@
+/*
+Example 1:
+Input: N = 3
+Output:
+* * *
+* * *
+* * *
+
+Example 2:
+Input: N = 6
+Output:
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+* */
+package StriverPractice.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -1,32 +1,31 @@
 /*
-Example 1:
-Input: N = 3
-Output:
-* * *
-* * *
+Input Format: N = 3
+Result:
+*
+* *
 * * *
 
-Example 2:
-Input: N = 6
-Output:
-* * * * * *
-* * * * * *
-* * * * * *
-* * * * * *
-* * * * * *
+Input Format: N = 6
+Result:
+*
+* *
+* * *
+* * * *
+* * * * *
 * * * * * *
 
-* */
-package StriverPractice;
+ */
+
+package StriverPractice.Patterns;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern2 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
