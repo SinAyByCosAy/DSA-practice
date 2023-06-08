a1451cb6bfa54802656e5c00c15d883c3938a2f9
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-08 17:10:25 +0530

    Pattern 7 done

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -1,4 +1,19 @@
 package StriverPractice.Patterns;
 
+import java.util.Scanner;
+
 public class Pattern7 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
