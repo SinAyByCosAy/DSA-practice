3ac3e9f32d0fa0580997037f6975b759d3dbc6eb
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-08 12:34:30 +0530

    Pattern 5 done

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,32 @@
+/*
+Input Format: N = 3
+Result:
+* * *
+* *
+*
+
+Input Format: N = 6
+Result:
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+ */
+package StriverPractice.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
