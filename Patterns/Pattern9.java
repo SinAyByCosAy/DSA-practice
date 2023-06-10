0645c17a93b2f9c801bcaa3de73c5c00132eb519
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-10 09:02:13 +0530

    Pattern 9 done

--- Patterns/Pattern9.java
+++ Patterns/Pattern9.java
@@ -1,11 +1,20 @@
 /*
 Input Format: N = 3
 Result:
+  *
+ ***
+*****
 *****
  ***
   *
 Input Format: N = 6
 Result:
+     *
+    ***
+   *****
+  *******
+ *********
+***********
 ***********
  *********
   *******
@@ -17,15 +26,24 @@ package StriverPractice.Patterns;
 
 import java.util.Scanner;
 
-public class Pattern8 {
+public class Pattern9 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<i;j++){
+            for(int j=i;j<n;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=2*n-(2*i-1);j++){
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=(2*(n-1))-(2*i-1);j++){
                 System.out.print("* ");
             }
             System.out.println();
