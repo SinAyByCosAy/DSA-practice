6692b1769614c148cc765523dd4cea22a78d3efd
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-12 17:08:21 +0530

    Pattern 20 done

--- Patterns/Pattern20.java
+++ Patterns/Pattern20.java
@@ -1,48 +1,35 @@
 /*
 Input Format: N = 3
 Result:
-******
-**  **
-*    *
 *    *
 **  **
 ******
+**  **
+*    *
+
 
 Input Format: N = 6
 Result:
-************
-*****  *****
-****    ****
-***      ***
-**        **
-*          *
 *          *
 **        **
 ***      ***
 ****    ****
 *****  *****
 ************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
  */
 package StriverPractice.Patterns;
 
 import java.util.Scanner;
 
-public class Pattern19 {
+public class Pattern20 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=i;j<=n;j++){
-                System.out.print("* ");
-            }
-            for(int k=1;k<i;k++){
-                System.out.print("    ");
-            }
-            for(int j=i;j<=n;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
@@ -51,6 +38,18 @@ public class Pattern19 {
                 System.out.print("    ");
             }
             for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print("* ");
+            }
+            for(int k=i;k<n;k++){
+                System.out.print("    ");
+            }
+            for(int j=i;j>=1;j--){
                 System.out.print("* ");
             }
             System.out.println();
