e3f45e4235b84e6a57d53f281b6116f262f42224
TanaySrivas <tanaysriofficial@gmail.com>
2023-06-12 20:44:10 +0530

    Pattern 21 done

---
+++ Patterns/Pattern21.java
@@ -0,0 +1,36 @@
+/*
+Input Format: N = 3
+Result:
+***
+* *
+***
+
+Input Format: N = 6
+Result:
+******
+*    *
+*    *
+*    *
+*    *
+******
+ */
+package StriverPractice.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern21 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
