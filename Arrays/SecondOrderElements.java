ff5405b283ccc4b5f6fbb88a8eb948f859c5b88b
TanaySrivas <tanaysriofficial@gmail.com>
2023-07-01 00:21:53 +0530

    Second order elements done

---
+++ Arrays/SecondOrderElements.java
@@ -0,0 +1,38 @@
+//Find second largest and second smallest elements
+package StriverPractice.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondOrderElements {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter array elements");
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE, secondSmallest = Integer.MAX_VALUE;
+        int result[] = new int[2];
+        for(int i=0;i<n;i++){
+            if(a[i] >= largest){
+                secondLargest = largest;
+                largest = a[i];
+            }else if(a[i] > secondLargest){
+                secondLargest = a[i];
+            }
+            if(a[i] <= smallest){
+                secondSmallest = smallest;
+                smallest = a[i];
+            }else if(a[i] < secondSmallest){
+                secondSmallest = a[i];
+            }
+        }
+        result[0] = secondLargest;
+        result[1] = secondSmallest;
+        System.out.println(Arrays.toString(result));
+    }
+}
